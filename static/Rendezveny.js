09056672e5867a56aa8e3b90c9cb8b84f7dc9dcc
Velican László <velicanlaszlo01@gmail.com>
2022-04-24 22:58:32 +0300

    lab3_3

--- static/Rendezveny.js
+++ static/Rendezveny.js
@@ -1,32 +1,26 @@
-function save() {                               //biztonság kedvéért vizsgálja hogy ne legyen egyik mező sem üres                              
+function save() {                               // biztonság kedvéért vizsgálja hogy ne legyen egyik mező sem üres
+  if (document.getElementById('form-rendezvenyNev').value !== '' && document.getElementById('form-rendezvenyKezdesiIdopont').value !== '' && document.getElementById('form-rendezvenyVegzesiIdopont').value !== '' && document.getElementById('form-rendezvenyHelyszine').value !== '' && document.getElementById('form-rendezvenySzervezok').value !== '') {
+    return true;
+  }
 
-    if (document.getElementById('form-rendezvenyNev').value !== '' && document.getElementById('form-rendezvenyKezdesiIdopont').value !== '' && document.getElementById('form-rendezvenyVegzesiIdopont').value !== '' && document.getElementById('form-rendezvenyHelyszine').value !== '' && document.getElementById('form-rendezvenySzervezok').value !== '') {
-        return true;
-    }
-    else {
-        alert("Mindegyik mezőt szükséges kitölteni!");
-        return false;
-    }
+  alert('Mindegyik mezőt szükséges kitölteni!');
+  return false;
 }
 
-function saveSzervezoCsatlakozas() {                               //biztonság kedvéért vizsgálja hogy ne legyen egyik mező sem üres                              
-
-    if (document.getElementById('form-rendezvenySzervezo').value !== '' && document.getElementById('form-rendezvenyID').value !== '') {
-        document.getElementById('form-rendezvenySzervezo').setValue = "";
-        document.getElementById('form-rendezvenyID').setValue = "";
-    }
-    else {
-        alert("Mindegyik mezőt szükséges kitölteni!");
-    }
+function saveSzervezoCsatlakozas() {                               // biztonság kedvéért vizsgálja hogy ne legyen egyik mező sem üres
+  if (document.getElementById('form-rendezvenySzervezo').value !== '' && document.getElementById('form-rendezvenyID').value !== '') {
+    document.getElementById('form-rendezvenySzervezo').setValue = '';
+    document.getElementById('form-rendezvenyID').setValue = '';
+  } else {
+    alert('Mindegyik mezőt szükséges kitölteni!');
+  }
 }
 
-function saveRendezvenyFenykepHozzaadas() {                               //biztonság kedvéért vizsgálja hogy ne legyen egyik mező sem üres        
-
-    if (document.getElementById('form-rendezvenySzervezo').value !== '' && document.getElementById('form-rendezvenyID').value !== '' && document.getElementById('form-rendezvenyFenykep').value!= '') {
-        document.getElementById('form-rendezvenySzervezo').setValue = "";
-        document.getElementById('form-rendezvenyID').setValue = "";
-    }
-    else {
-        alert("Mindegyik mezőt szükséges kitölteni!");
-    }
+function saveRendezvenyFenykepHozzaadas() {                               // biztonság kedvéért vizsgálja hogy ne legyen egyik mező sem üres
+  if (document.getElementById('form-rendezvenySzervezo').value !== '' && document.getElementById('form-rendezvenyID').value !== '' && document.getElementById('form-rendezvenyFenykep').value != '') {
+    document.getElementById('form-rendezvenySzervezo').setValue = '';
+    document.getElementById('form-rendezvenyID').setValue = '';
+  } else {
+    alert('Mindegyik mezőt szükséges kitölteni!');
+  }
 }
